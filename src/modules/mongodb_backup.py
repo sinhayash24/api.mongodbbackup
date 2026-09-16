098813cb952a27ac09eed167b059b6fcf8ab2707
Yash Raj <yashrajsinha24@gmail.com>
2026-09-16 19:47:46 +0530

    added method to take backup of db

--- src/modules/mongodb_backup.py
+++ src/modules/mongodb_backup.py
@@ -1,22 +1,49 @@
 import subprocess
+from datetime import date
 from flask import jsonify
+
+def perform_backup():
+
+    datestamp = date.today().isoformat()
+
+    backup_name = f"mongo_backup_{datestamp}"
+
+    subprocess.run(
+        [
+            "docker",
+            "exec",
+            "mongo",
+            "mongodump",
+            "--host=localhost:27017",
+            f"--out=/backups/{backup_name}"
+            "--gzip"
+        ],
+        check=True
+    )
+
+    subprocess.run(
+        [
+            "docker",
+            "cp",
+            f"mongo:/backups/{backup_name}",
+            "/home/azureuser/mongo-backups/."
+        ],
+        check=True
+    )
+
+    return {
+        "status": "success",
+        "message": f"Backup created successfully: {backup_name}"
+    }
+
 
 def register_routes(app):
 
     @app.route("/backup", methods=["POST"])
     def backup():
 
-        try:
-            result = subprocess.run(
-                ["docker", "ps"],
-                capture_output=True,
-                text=True
-            )
-    
-            return jsonify({
-                "status": "success",
-                "output": result.stdout,
-            })
+        try:  
+            return jsonify(perform_backup())
 
         except Exception as ex:
 
